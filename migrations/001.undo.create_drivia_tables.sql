c3f1d2d43ea6dde7e12421e6f859ebadc4b0fafb
Jordan Henn <jordanwalkerhenn@gmail.com>
2020-09-24 22:55:51 -0700

    building mvp

--- migrations/001.undo.create_drivia_tables.sql
+++ migrations/001.undo.create_drivia_tables.sql
@@ -1,6 +1,6 @@
-DROP TABLE IF EXISTS drivia_questions;
+DROP TYPE IF EXISTS trivia_category;
 
-DROP TYPE IF EXISTS drivia_category;
+DROP TABLE IF EXISTS drivia_questions;
 
 DROP TABLE IF EXISTS drivia_leaderboard;
 
